b3b7fcc84aaf46321304a3a9d4b54363e14a77ce
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2025-11-18 22:26:24 -0300

    feat: Enhance home page with logout functionality and form validation

--- public/js/home.js
+++ public/js/home.js
@@ -4,6 +4,9 @@ const myModal = new bootstrap.Modal(
 let logged = sessionStorage.getItem('logged')
 const session = localStorage.getItem('session')
 
+let data = {
+  transactions: [],
+}
 const checkLogged = () => {
   const session = localStorage.getItem('session')
 
@@ -19,4 +22,40 @@ const checkLogged = () => {
   }
 }
 
+const dataUser = localStorage.getItem(logged)
+if (dataUser) {
+  data = JSON.parse(dataUser)
+}
+
 checkLogged()
+
+const logout = () => {
+  sessionStorage.removeItem('logged')
+  localStorage.removeItem('session')
+
+  window.location.href = 'index.html'
+}
+
+document.getElementById('button-logout').addEventListener('click', logout)
+const saveData = (data) => {
+  localStorage.setItem(data.login, JSON.stringify(data))
+}
+document.getElementById('transaction-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const value = parseFloat(document.getElementById('value-input').value)
+  const description = document.getElementById('description-input').value
+  const date = document.getElementById('date-input').value
+  const type = document.querySelector('input[name="type-input"]:checked').value
+
+  data.transactions.unshift({
+    value,
+    description,
+    date,
+    type,
+  })
+  saveData(data)
+
+  myModal.hide()
+  e.target.reset()
+})
